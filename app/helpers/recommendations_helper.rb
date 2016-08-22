2df40c75133d527ab0d672bc5f0f688129840613
CarlosGomes94290 <madureiracharles@gmail.com>
2016-08-22 15:46:59 +0100

    add description card

--- app/helpers/recommendations_helper.rb
+++ app/helpers/recommendations_helper.rb
@@ -1,2 +1,8 @@
 module RecommendationsHelper
+
+  def short_description(description)
+   end_of_short_description = description.length > 249 ? (description.index(' ', 249)) : description.length
+   description.slice(0, end_of_short_description) + " (...)"
+ end
+
 end
