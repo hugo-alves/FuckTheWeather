9a61ca0f33b136115c2603ddc16875d4d0e991ce
CarlosGomes94290 <madureiracharles@gmail.com>
2016-08-24 15:03:44 +0100

    add page show for user

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/show'
+
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
